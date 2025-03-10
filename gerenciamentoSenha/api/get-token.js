27d276163a7f9e30e2e9374f0b4daf0efb87a9e0
bowie <marketing@letscode.com.br>
2025-03-10 04:50:53 -0300

    Descrição das mudanças

--- gerenciamentoSenha/api/get-token.js
+++ gerenciamentoSenha/api/get-token.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     }
 
     const data = await response.json();
-    res.status(200).json(data); // Retorna o token
+    res.status(200).json(data); // Retorna o token de acesso
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
